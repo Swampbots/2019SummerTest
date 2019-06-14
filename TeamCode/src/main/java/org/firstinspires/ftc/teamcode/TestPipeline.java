75de1f48304b726d9d00005c2bab66959c409099
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-06-13 23:50:21 -0400

    Deactivated unused import statement
    I'm not sure why this is in here and not sure why it's not being located properly.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPipeline.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+//import java.util.stream.Collectors;
 import java.util.HashMap;
 
 import org.opencv.core.*;
