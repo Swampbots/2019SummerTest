fe3b14765c56d0d38eb42144f1ee331664df1352
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-06-16 13:58:23 -0400

    Added Anton_The_Manthon
    No comment.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Anton_The_Manthon.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp( name = "VEX IQ", group = "Auto decc Inventnator" )
+public class Anton_The_Manthon extends OpMode {
+
+    public void init() {
+        // weeeoooee
+    }
+
+    public void loop() {
+        telemetry.addLine("Anthon the Manthon");
+        telemetry.update();
+    }
+}
