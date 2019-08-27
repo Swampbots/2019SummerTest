2b291b148fc69c3d04a368a5314e908263412d72
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-08-27 15:43:29 -0400

    Added init() and loop() methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTileRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTileRunner.java
@@ -1,5 +1,13 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 public class TestTileRunner extends OpMode {
 
+    public void init(){
+
+    }
+    public void loop(){
+
+
+    }
 
 }
