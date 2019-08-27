21b1e1ccee5d0276f89edb34fbf6032dea1234f2
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-08-27 15:44:10 -0400

    Added drive motors and telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTileRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTileRunner.java
@@ -1,13 +1,31 @@
 package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 public class TestTileRunner extends OpMode {
 
+    public DcMotor rlDrive;
+    public DcMotor rrDrive;
+    public DcMotor flDrive;
+    public DcMotor frDrive;
+
     public void init(){
+        rlDrive = hardwareMap.dcMotor.get("rl_drive");
+        rrDrive = hardwareMap.dcMotor.get("rr_drive");
+        flDrive = hardwareMap.dcMotor.get("fl_drive");
+        frDrive = hardwareMap.dcMotor.get("fr_drive");
 
     }
     public void loop(){
 
 
+        telemetry.addData("rlDrive power", rlDrive.getPower());
+        telemetry.addData("rrDrive power", rrDrive.getPower());
+        telemetry.addData("flDrive power", flDrive.getPower());
+        telemetry.addData("frDrive power", frDrive.getPower());
+        telemetry.update();
     }
 
 }
