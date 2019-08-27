df2d5d3a1604295c6252edfd7dd55835633a86c9
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-08-27 15:44:32 -0400

    Added @TeleOp annotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTileRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTileRunner.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+@TeleOp(name= "TileRunner Test")
 public class TestTileRunner extends OpMode {
 
     public DcMotor rlDrive;
