88a806ab276b1b17dff25dd8c42d5cc09cde75c4
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-08-27 15:47:25 -0400

    Added methods and @TeleOp annotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestLift.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@TeleOp(name = "Lift Test")
 public class TestLift extends OpMode {
 
+    public void init() {
+    }
+    public void loop() {
+    }
 }
