2845d0b86c8e6cb131a9ad5fdd9bd9fa7bd72224
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-08-28 14:53:31 -0400

    Disabled TestPipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestPipeline.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.corningrobotics.enderbots.endercv.CameraViewDisplay;
 
+@Disabled
 @TeleOp(name = "Pipeline Test", group = "Testing")
 public class TestPipeline extends OpMode {
 
