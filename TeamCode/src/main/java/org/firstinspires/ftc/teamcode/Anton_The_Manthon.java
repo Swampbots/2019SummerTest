ef7130bdbfec55341b2ced51ce7234626bb7eaf2
Blake Sanders <ac3.train3r.blak3@gmail.com>
2019-08-28 14:53:13 -0400

    Disabled Anthon_The_Manthon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Anton_The_Manthon.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Anton_The_Manthon.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@Disabled
 @TeleOp( name = "VEX IQ", group = "Auto decc Inventnator" )
 public class Anton_The_Manthon extends OpMode {
 
